aa57cba46555dd7470e4c3b32ecbb531aa379150
Ryan Pairitz <ryn.pair@me.com>
2022-10-13 11:46:04 -0400

    initial email button animation w/ react spring

--- src/Components/Contact/EmailButton.js
+++ src/Components/Contact/EmailButton.js
@@ -1,24 +1,33 @@
 import { useState } from 'react';
 import { ReactComponent as EmailIcon } from '../img/icons_email.svg';
+import { useSpring, animated } from "react-spring";
+import useMeasure from "react-use-measure";
 import './Contact.css';
 
 const EmailButton = () => {
+    const [ref, bounds] = useMeasure();
     const [showEmail, setShowEmail] = useState(false);
+    const buttonStyles = useSpring({
+        width: showEmail ? (bounds.width + 13) : 13
+    });
 
     return (
-        <a className='button'
+        <animated.a className='button'
             onMouseEnter={() => setShowEmail(true)}
             onMouseLeave={() => setShowEmail(false)}
             href={showEmail ? "mailto:ryn.pair@me.com" : "#"}
+            style={buttonStyles}
         >
-            {showEmail ?
-                <span className='button-text'>
+            <EmailIcon className="email-icon"/>
+            {showEmail &&
+                <span
+                    className='button-text'
+                    ref={ref}
+                >
                     ryn.pair@me.com
                 </span>
-                : <></>
             }
-            <EmailIcon className="email-icon" />
-        </a>
+        </animated.a>
     );
 };
 
