f798f67b767a8fddd31044bf35e8bf5e228fd9e6
Ryan Pairitz <ryn.pair@me.com>
2022-10-17 15:51:31 -0400

    animated link works w/ children w/ different tags

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -1,7 +1,6 @@
 import { ReactComponent as GoldenParrot } from "./goldenParrot.svg";
 import { ReactComponent as TwitterIcon } from "../../img/icons_twitter.svg";
 import Contact from "../../Contact/Contact";
-import SpecialLink from "../../Links/SpecialLink";
 import AnimatedLink from "../../Links/AnimatedLink";
 import "./About.css";
 
@@ -14,13 +13,20 @@ const About = () => {
             </div>
             <div className="about-text-container">
                 <p>
-                    <a href="https://twitter.com/ryanparrots" target="_blank" rel="noopener noreferrer">
-                        <b>
-                            Hi! I’m Ryan Pairitz &#40;<TwitterIcon className="icon" />&#41;
-                        </b>
-                    </a>, a designer, developer, and animator. I specialize in user experience, frontend development, and design, and I have been doing freelance work since I was 14.</p>
+                    <AnimatedLink href="https://twitter.com/ryanparrots" hasManyTypes={true}>
+                        {"Hi! I'm Ryan Pairitz (".split('').map((value, index) => (
+                            value === ' ' ?
+                            <b key={index}>&nbsp;</b>
+                            :
+                            <b key={index}>{value}</b>
+                        ))}
+                        <TwitterIcon className="icon" />
+                        <b>&#41;</b>
+                    </AnimatedLink>, a designer, developer, and animator. I specialize in user experience, frontend development, and design, and I have been doing freelance work since I was 14.</p>
                 <p>I studied Computer Science &#38; Engineering at the University of Notre Dame, where I was selected to serve as a designer for&nbsp;
-                    <AnimatedLink to="#">The&nbsp;Shirt</AnimatedLink> — the highest-selling piece of collegiate apparel in the nation.</p>
+                    <AnimatedLink to="#">
+                        The Shirt
+                    </AnimatedLink> — the highest-selling piece of collegiate apparel in the nation.</p>
                 <p>I have designed and developed multiple apps for web or mobile. One of which,&nbsp;
                     <AnimatedLink>
                         Partake
--- src/Components/Links/AnimatedLink.js
+++ src/Components/Links/AnimatedLink.js
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import { Children, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { animated, useTrail, useSpring } from "react-spring";
 import './Link.css';
 
-const AnimatedLink = ({ to, children }) => {
-    const chars = children.split("").map((value, index) => (
-        { key: index, char: value }
-    ));
+const AnimatedLink = ({ to, href, hasManyTypes, children }) => {
+    const items = (hasManyTypes ? Children.toArray(children) :
+        children.split("").map((value, index) => (
+            { key: index, char: value }
+        ))
+    );
     const [active, setActive] = useState(false);
-    const trail = useTrail(chars.length, {
+    const trail = useTrail(items.length, {
         loop: { reverse: active },
         opacity: active ? 0.38 : 1,
         transform: active ? 'translateY(-3px)' : 'translateY(0px)',
@@ -28,17 +30,57 @@ const AnimatedLink = ({ to, children }) => {
     return (
         <animated.span className="link-container"
         style={linkStyle}>
-            <Link onMouseEnter={() => setActive(true)}
-                onMouseLeave={() => setActive(false)}
-                className="link-text"
-                to={to}
-            >
-                {trail.map((style, index) => (
-                    <animated.b key={index} style={style}>
-                        {chars[index].char}
-                    </animated.b>
-                ))}
-            </Link>
+            {href ?
+                <a onMouseEnter={() => setActive(true)}
+                    onMouseLeave={() => setActive(false)}
+                    className="link-text"
+                    href={href}
+                    target="_blank" rel="noopener noreferrer"
+                >
+                    {hasManyTypes ?
+                        trail.map((style, index) => (
+                            <animated.span key={index} style={style}>
+                                {items[index]}
+                            </animated.span>
+                        ))
+                        :
+                        trail.map((style, index) => (
+                            items[index].char === ' ' ?
+                                <animated.b key={index} style={style}>
+                                    &nbsp;
+                                </animated.b>
+                                :
+                                <animated.b key={index} style={style}>
+                                    {items[index].char}
+                                </animated.b>
+                        ))
+                    }
+                </a> :
+                <Link onMouseEnter={() => setActive(true)}
+                    onMouseLeave={() => setActive(false)}
+                    className="link-text"
+                    to={to}
+                >
+                    {hasManyTypes ?
+                        trail.map((style, index) => (
+                            <animated.span key={index} style={style}>
+                                {items[index]}
+                            </animated.span>
+                        ))
+                        :
+                        trail.map((style, index) => (
+                            items[index].char === ' ' ?
+                                <animated.b key={index} style={style}>
+                                    &nbsp;
+                                </animated.b>
+                                :
+                                <animated.b key={index} style={style}>
+                                    {items[index].char}
+                                </animated.b>
+                        ))
+                    }
+                </Link>
+            }
         </animated.span>
     );
 };
